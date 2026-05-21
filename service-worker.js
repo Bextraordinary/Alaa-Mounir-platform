dcccb703aab6c5d785b462dba1c2e7fe0eae78c4
Bextraordinary <badrlord7070@gmail.com>
2026-05-21 23:13:25 +0200

    Update service worker cache and URLs

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,24 @@
-const CACHE_NAME = 'alaa-v1';
-const urlsToCache = ['/', '/index.html', '/style.css', '/script.js', '/manifest.json', '/ss.png'];
+const CACHE_NAME = "alaa-munir-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./ss.png",
+  "./nn.png",
+  "./w.jpg"
+];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
